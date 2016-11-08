036f413894093938b18ebc622388ffdad9ba1b49
hekemp <heather-kemp@uiowa.edu>
2016-11-07 21:46:04 -0600

    Added more functionality to mock

--- Master/Order.java
+++ Master/Order.java
@@ -1,7 +1,20 @@
 
-public class Order {
+public class Order implements Event{
 
-	public Order(){
-	
-	}
+    public Order(){
+    
+    }
+    
+    public void performAction(String Method){
+        System.out.println("Order did something.");
+    }
+    
+    public Event getEvent(){
+
+         return (Event) this;
+       }
+            
+    public String getPara(){
+        return "Null";
+       }
 }
