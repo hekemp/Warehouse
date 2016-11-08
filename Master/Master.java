895a55334243111ad1bd6929638e9707221aecca
hekemp <heather-kemp@uiowa.edu>
2016-11-07 21:45:40 -0600

    Added code for running simulation and test cases

--- Master/Master.java
+++ Master/Master.java
@@ -4,25 +4,47 @@ import java.util.*;
 public class Master
 {
     // instance variables - replace the example below with your own
-    private PriorityQueue<Event> myEvents;
-    //private PriorityQueue<Object[]> myParameters;
+    private ArrayList<Event> myEvents;
+    private ArrayList<String> myParameters;
     
 
     public Master()
-    {myEvents = new PriorityQueue<Event>();
-
+    {myEvents = new ArrayList<Event>();
+     myParameters = new ArrayList<String>();
     }
     
-    public void addEvent(Event newEvent)
+    public void addEvent(Event newEvent, String parameters)
     {myEvents.add(newEvent);
+     myParameters.add(parameters);
     }
     
-    //public void addParameters(Object... args)
-    //{myParameters.add(args);
-    //}
     
-    public static void runSimulation()
-    {
+    public void runSimulation()
+    {Belt myBelt = new Belt();
+     Floor myFloor = new Floor();
+     Inventory myInventory = new Inventory();
+     Order myOrder = new Order();
+     Robot myRobot = new Robot(0,0,0);
+     Shelf myShelf = new Shelf();
+     Visualizer myVisualizer = new Visualizer();
+     
+     this.addEvent((Event)myBelt," ");
+     this.addEvent((Event)myFloor, " ");
+     this.addEvent((Event)myInventory," ");
+     this.addEvent((Event)myOrder, " ");
+     this.addEvent((Event)myRobot," ");
+     this.addEvent((Event)myShelf, " ");
+     this.addEvent((Event)myVisualizer," ");
+     
+     while(!myEvents.isEmpty())
+     {Event newEvent = myEvents.remove(0);
+      String newPara = myParameters.remove(0);
+      if(newPara != "Null")
+      {newEvent.performAction(newPara);
+      myEvents.add(newEvent.getEvent());
+      myParameters.add(newEvent.getPara());}
+     }
+     
     }
     
     // Unit tests with mock objects can test whether queueing new events works, 
