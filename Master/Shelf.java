7d378c4d2eb3910b7b6179465641ce0abf661adf
hekemp <heather-kemp@uiowa.edu>
2016-11-07 21:47:03 -0600

    Added more functionality to mock

--- Master/Shelf.java
+++ Master/Shelf.java
@@ -1,7 +1,20 @@
 
-public class Shelf {
+public class Shelf implements Event{
 
 	public Shelf(){
 		
 	}
+	
+	public void performAction(String Method){
+		System.out.println("Shelf did something.");
+	}
+	
+	public Event getEvent(){
+
+	     return (Event) this;
+	   }
+	        
+	public String getPara(){
+	    return "Null";
+	   }
 }
