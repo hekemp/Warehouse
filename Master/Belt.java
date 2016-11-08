19b4bbee2e67a6ad9d7542a34c1e7c79f3da1d9f
hekemp <heather-kemp@uiowa.edu>
2016-11-07 21:43:08 -0600

    Added more functionality to mock

--- Master/Belt.java
+++ Master/Belt.java
@@ -1,7 +1,18 @@
 
-public class Belt {
-	
+public class Belt implements Event{
 	public Belt(){
-		
 	}
+	
+	public void performAction(String Method){
+	    System.out.println("Belt did something.");
+	}
+	
+	public Event getEvent(){
+
+	     return (Event) this;
+	   }
+	        
+	public String getPara(){
+	    return "Null";
+	   }
 }
