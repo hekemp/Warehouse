302f088540ee938572ee6dcfa5c909a5ece08211
hekemp <heather-kemp@uiowa.edu>
2016-11-07 21:47:23 -0600

    Added more functionality to mock

--- Master/Visualizer.java
+++ Master/Visualizer.java
@@ -1,8 +1,20 @@
 
-public class Visualizer {
+public class Visualizer implements Event{
 	
 	public Visualizer(){
 		
 	}
 
+	public void performAction(String Method){
+		System.out.println("Visualizer did something");
+	}
+	
+	public Event getEvent(){
+
+	     return (Event) this;
+	   }
+	        
+	public String getPara(){
+	    return "Null";
+	   }
 }
