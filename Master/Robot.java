f43ddc24a19e4ec2307b375af48929f9344536e5
hekemp <heather-kemp@uiowa.edu>
2016-11-07 21:46:33 -0600

    Added more functionality to robot

--- Master/Robot.java
+++ Master/Robot.java
@@ -1,31 +1,64 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Robot implements Event {
 
-	private int myID;
-	private int myX;
-	private int myY;
-	private ArrayList<int[]> myMovements;
+    private int myID;
+    private int myX;
+    private int myY;
+    private ArrayList<int[]> myMovements;
+    private PriorityQueue<Event> myEvents;
+    private PriorityQueue<String> myParameters;
 
-	public Robot(int x, int y, int ID){
-		myX = x;
-		myY = y;
-		myMovements = new ArrayList<int[]>();
-		myID = ID;
-	}
-	
-	public void move(int x, int y){
-		System.out.println("Robot " + myID + " has moved: X = "+myX+ " Y = "+myY+".");
-		myX = x;
-		myY = y;
-	}
+    public Robot(int x, int y, int ID){
+        myX = x;
+        myY = y;
+        myMovements = new ArrayList<int[]>();
+        myID = ID;
+        myEvents = new PriorityQueue<Event>();
+        myParameters = new PriorityQueue<String>();
+    }
+    
+    public void move(int x, int y){
+        if(x == 1 && y == 1)
+            {
+            myX = x;
+            myY = y;
+            System.out.println("Robot " + myID + " has moved: X = "+myX+ " Y = "+myY+".");
+            System.out.println("New move enqueued");
+            myEvents.add(this);
+            myParameters.add("move,1,1");}
+        else
+            {myX = x;
+            myY = y;
+            System.out.println("Robot " + myID + " has moved: X = "+myX+ " Y = "+myY+".");}
+            
+    }
+    
+    public Event getEvent(){
+    if(myEvents.isEmpty())
+        return (Event) new Robot(1,1,1);
+    else
+        return myEvents.remove();   
+    
+    }
+    
+    public String getPara(){
+        if(myParameters.isEmpty())
+            return "Null";
+        else
+            return myParameters.remove();   
+        
+        }
 
-	@Override
-	public void performAction(String Method) {
-		
-		
-	}
-	
-	
-	/// move method moves the 1/1 but then enques another move with x,y
+    @Override
+    public void performAction(String Method) {
+            if(Method == "move,1,1")
+                this.move(2, 2);
+            else
+                this.move(1, 1);
+        
+    }
+    
+    
+    /// move method moves the 1/1 but then enques another move with x,y
 }
