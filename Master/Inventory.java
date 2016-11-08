96a74b4de1751402657ab0a9ac563f21f01f8aaa
hekemp <heather-kemp@uiowa.edu>
2016-11-07 21:44:43 -0600

    Added more functionality to mock

--- Master/Inventory.java
+++ Master/Inventory.java
@@ -1,7 +1,19 @@
 
-public class Inventory {
+public class Inventory implements Event{
 
 	public Inventory(){
 	
 	}
+	public void performAction(String Method){
+		System.out.println("Inventory did something");
+	}
+	
+	public Event getEvent(){
+
+	     return (Event) this;
+	   }
+	        
+	public String getPara(){
+	    return "Null";
+	   }
 }
