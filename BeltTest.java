744d54839729ef6e6882967e687138f690a39491
melkeurti <mounaikram-elkeurti@uiowa.edu>
2016-11-07 19:35:49 -0600

    Add files via upload

---
+++ BeltTest.java
@@ -0,0 +1,21 @@
+package project;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class BeltTest {
+
+	@Test
+	public void test() {
+		Belt belt = new Belt();
+		belt.changeSpeed(5.0);
+		//Assert.assertEquals(5.0, 0.0);
+		
+	}
+	
+
+}
