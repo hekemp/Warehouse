14b29a6693e94e5df0f8285717a0d53df85368c8
hekemp <heather-kemp@uiowa.edu>
2016-11-07 21:44:08 -0600

    Added more functionality to mock

--- Master/Floor.java
+++ Master/Floor.java
@@ -1,8 +1,19 @@
 
-public class Floor {
+public class Floor implements Event{
 
 	public Floor(){
 	}
 	
+	public void performAction(String Method){
+		System.out.println("Floor did something");
+	}
 	
+	public Event getEvent(){
+
+	     return (Event) this;
+	   }
+	        
+	public String getPara(){
+	    return "Null";
+	   }
 }
