2c309c51228dabe1af648964cce2e2537b4f46d3
rschne <rachel-schneberger@uiowa.edu>
2016-11-12 11:51:48 -0600

    Update Robot.java
    start of javadoc comments

--- Robot.java
+++ Robot.java
@@ -8,6 +8,10 @@ public class Robot {
 	private int xCord;
 	private int yCord;
 	
+	/*
+	* @author Rachel Schneberger
+	* Robot constructor that sets a starting location, its id and a full charge
+	*/
 	public Robot(int x, int y, int id, int charge ){
 		xCord = x;
 		yCord = y;
@@ -15,6 +19,11 @@ public class Robot {
 		robotCharge = charge;
 	}
 	
+	/*
+	* @author Rachel Schneberger
+	* @param x, the x location on the floor grid 
+	* @param y, the y location on the floor grid
+	*/
 	public void move(int x, int y){
 		/*
 		 * needs to check to see if space given is occupied
@@ -25,6 +34,12 @@ public class Robot {
 		yCord = y;
 	}
 	
+	/*
+	* @author Rachel Schneberger
+	* @param usable a boolean that tells the floor if a robot is usable or not
+	* @param robot charge an int that keeps track of a robots charge and will decrease when
+	* robot is being used and when robot is being charged.
+	*/
 	public void restAndCharge(){
 		//robots cannot be used while charging
 		// charge increases by one every clock tick...
@@ -35,6 +50,9 @@ public class Robot {
 		usable = true;
 	}
 	
+	/*
+	* @author Rachel Schneberger
+	*/
 	public void chargeUsage(){
 		//when a robot is usable = true
 		//charge needs to decrease by one every clock tick
@@ -49,6 +67,9 @@ public class Robot {
 		}
 	}
 	
+	/*
+	* @author Rachel Schneberger
+	*/
 	public void pickShelf(){
 		//wait one clock tick before picking up and moving
 		shelf = true;
