774867c5109aa8a8de0bb8847a083c6fbfd10dfe
Qiansan <xinyu-qian@uiowa.edu>
2016-11-13 14:08:22 -0600

    interface for visualizer from floor

---
+++ viewFloor.java
@@ -0,0 +1,12 @@
+
+// interface for visualizer
+public interface viewFloor {
+	public Point[] getBelt();
+	public Point[] getShelf();
+	public Point getPacker();
+	public Point getPicker();
+	public Point getReceving();
+	public Point getCharger();
+	public Point shipping();
+	public int floorSize();
+}
