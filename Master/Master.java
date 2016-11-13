d55d2034c3eae0e65eb0bec292ee433d8dccd76b
hekemp <heather-kemp@uiowa.edu>
2016-11-13 10:12:51 -0600

    Add javadoc

--- Master/Master.java
+++ Master/Master.java
@@ -1,24 +1,47 @@
 import java.util.*;
 
+/**
+ * 
+ * @author Heather Kemp
+ * The Master class is like the "main" method, which controls the simulated warehouses' runs.
+ * It manages the instances of the Floor, Robot, Inventory, Order, Belt, Visualizer, and other classes as required.
+ * It has a method to addEvent to update it's internal "queues" for the events and its parameters.
+ * It also includes an event runSimulation, which runs all Events using the provided Event interface (provided in a seperate java file
+ *
+ */
 
 public class Master
 {
-    // instance variables - replace the example below with your own
+    
     private ArrayList<Event> myEvents;
     private ArrayList<String> myParameters;
     
 
+    /**
+     * Constructor initializes the queues for the events
+	 */
     public Master()
     {myEvents = new ArrayList<Event>();
      myParameters = new ArrayList<String>();
     }
     
+     /**
+     * Add an event. This code allows us to add to both queues at the same time, and if an item has a Master object
+     * down the line, it will be able to access the private queues.
+     *
+     * @input newEvent - an Event type object for which to execute a method on
+     * @input parameters - a String object which contains the internal method to execute and any parameters
+	 */
     public void addEvent(Event newEvent, String parameters)
     {myEvents.add(newEvent);
      myParameters.add(parameters);
     }
     
-    
+    /**
+     * Run simulation. This code creates instances of all of the types of objects. It also executes very simple
+     * commands to each event to start the cycle of events for the day.
+     *
+	 */
     public void runSimulation()
     {Belt myBelt = new Belt();
      Floor myFloor = new Floor();
@@ -46,10 +69,5 @@ public class Master
      }
      
     }
-    
-    // Unit tests with mock objects can test whether queueing new events works, 
-    // initializing the queue with some initial events, and of course creating 
-    // instances of the other (mock) components.
-    
     
 }
