6f86f52423d4d5a7f285677dc051a95e233b5394
hyo kyung kang <hyokyung-kang@uiowa.edu>
2016-11-13 13:47:20 -0500

    Update Orders.java

--- Warehouse/src/warehouse/Orders.java
+++ Warehouse/src/warehouse/Orders.java
@@ -1,10 +1,45 @@
 package warehouse;
 
+import java.util.HashMap;
+
 public class Orders {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	String progress;
+	String address;
+	String dateReceived;
+	int orderNumber;
+	HashMap<String, Integer> Item;
+	public Orders(String progress,String address, String dateReceived,int orderNumber){
+		progress=progress;
+		address=address;
+		dateReceived=dateReceived;
+		this.orderNumber=orderNumber;
+		this.Item= new HashMap<String,Integer>();
+		
 	}
-
+	
+	
+	
+	public void ship(String dateReceived){
+	
+	}
+	public void cancel(){
+		
+	}
+	
+	public void changeProgress(){
+		
+	}//
+	public void returningprocess(){
+		
+	}
+	public void addItems(){
+		
+	}
+	public void getProgress(){
+		
+	}//
+	
+	
 }
+
