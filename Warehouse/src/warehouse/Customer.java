e78d19afc0de33e3cff7ecad79c5e92aa501d8ee
hyo kyung kang <hyokyung-kang@uiowa.edu>
2016-11-13 12:48:57 -0600

    Add files via upload

---
+++ Warehouse/src/warehouse/Customer.java
@@ -0,0 +1,13 @@
+package warehouse;
+
+public class Customer {
+	String cusname;
+	int ID;
+	int password;
+	
+ public Customer(String Username,int ID,int password){
+	 cusname=Username;
+	 this.ID=ID;
+	 this.password=password;
+ }
+}
