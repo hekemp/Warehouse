bed1fb4f4f096e4c4513e3fe6e9a1c5afbed81ab
Xinglzhao <xingluzhao2015@gmail.com>
2016-11-14 19:50:14 -0600

    Update MockBelt.java

--- Visualizer/src/main/java/MockBelt.java
+++ Visualizer/src/main/java/MockBelt.java
@@ -7,15 +7,45 @@
 /**
  *
  * @author zhaoxinglu
+ * This class is used to test only
  */
 public class MockBelt {
+  
     Point item;
+    boolean ismove;
+  /**
+ *
+ * @author zhaoxinglu
+ * @param Point, which represent the coordinates of item on the belt
+ * @param boolean, we can know whether the item on the belt is move or not
+ * These two provide useful information to Visulaizer 
+ */
     public MockBelt(){
         item=new Point(0,3);
+        ismove=true;
     }
-    
+ /**
+ *
+ * @author zhaoxinglu
+ * A constructor for mock belt 
+ * 
+ */
     public Point TrackItem(){
     return item;
 }
-    
+ /**
+ *
+ * @author zhaoxinglu
+ * @return Point, the coordinate of item
+ * 
+ */
+    public void changemovestate(boolean t){
+        ismove=t;
+    }
+  /**
+ *
+ * @author zhaoxinglu
+ * @param boolean, which can be used to change the state of item
+ * 
+ */
 }
