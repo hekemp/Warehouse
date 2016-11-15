60cd46f02ee8d2e9ab93413082282723bf7a4d32
Xinglzhao <xingluzhao2015@gmail.com>
2016-11-14 19:54:37 -0600

    Update Point.java

--- Visualizer/src/main/java/Point.java
+++ Visualizer/src/main/java/Point.java
@@ -7,10 +7,17 @@
 /**
  *
  * @author zhaoxinglu
+ * point class helps to get coordinates of items
  */
 public class Point {
     int x;
     int y;
+ /**
+ + *
+ + * @author zhaoxinglu
+ + * two initializer, one can take parameters to set coordinates 
+ + * the other one defines exact coordinate numbers
+ + */
     public Point(int x, int y){
         this.x=x;
         this.y=y;
@@ -19,6 +26,14 @@ public class Point {
         this(5,7);
         
     }
+ /**
+ *
+ * @author zhaoxinglu
+ * @override toString, print out the x and y in a clear way
+ * @return String
+ */
+    
+    
     public String toString(){
         String s;
         s = String.format("(%d"+","+"%d)", x, y);
